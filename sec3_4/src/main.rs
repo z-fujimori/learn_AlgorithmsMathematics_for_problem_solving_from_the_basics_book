a61af18734ce8c9c474f36f0fc5b4f3543c7ee93
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-02-13 01:13:21 +0900

    3.7問題028

--- sec3_4/src/main.rs
+++ sec3_4/src/main.rs
@@ -3,15 +3,22 @@ use proconio::input;
 fn main() {
     input!{
         n: i32,
+        h_vec: [i32; n],
     }
 
-    let mut ans = 1.0;
+    // let mut ans = 0;
+    let mut n_1 = (h_vec[0] - h_vec[1]).abs();
+    let mut n_2 = 0;
 
-    for i in 1..n {
-        // let r = (n-i) as f64 / n as f64;
-        ans += n as f64 / i as f64;
-        // println!("{}",ans);
+    for i in 2..h_vec.len() {
+        // println!("前半: {} {}", n_1, n_2);
+        let jump1 = n_1 + (h_vec[i] - h_vec[i-1]).abs();
+        let jump2 = n_2 + (h_vec[i] - h_vec[i-2]).abs();
+        // println!("jump1: {}, jump2: {}", jump1, jump2);
+        n_2 = n_1;
+        n_1 = if jump1 < jump2 { jump1 } else {jump2};
+        // println!("後半: {} {}\n", n_1, n_2);
     }
 
-    println!("{}", ans);
+    println!("{}", n_1);
 }
