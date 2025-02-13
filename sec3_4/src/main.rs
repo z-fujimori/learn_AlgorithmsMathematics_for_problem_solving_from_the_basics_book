0fc092d5901b1d40c9344eea8a69d8f3045e12ad
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-02-13 09:58:08 +0900

    030ナップザック

--- sec3_4/src/main.rs
+++ sec3_4/src/main.rs
@@ -1,20 +1,29 @@
+use std::cmp::max;
+
 use proconio::input;
 
 fn main() {
     input!{
-        n: i32,
-        // h_vec: [i32; n]
+        n: usize,
+        w: usize,
+        vw: [[i64; 2]; n]
     }
 
-    let mut n_2 = 1;
-    let mut n_1 = 1;
+    // println!("{:?} {} {}", vw, n, w);
 
-    for _i in 2..=n {
-        let pool = n_1;
-        n_1 = n_1 + n_2;
-        n_2 = pool;
+    let mut back = vec![vec![0; w+1]; n+1];
+
+    for i in 1..=n {
+        for j in 0..=w {
+            if j < vw[i-1][0] as usize {
+                back[i][j] = back[i-1][j];
+            } else {
+                // println!("{} {}",back[i-1][j], back[i-1][j-vw[i-1][0] as usize] + vw[i-1][1]);
+                back[i][j] = max(back[i-1][j], back[i-1][j-vw[i-1][0] as usize] + vw[i-1][1]);
+            }
+        }
     }
 
 
-    println!("{}", n_1);
+    println!("{}", back[n][w]);
 }
